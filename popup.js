e794d96ff8f557bc99a55e8330f7f530fdc13427
thooper159 <47039665+thooper159@users.noreply.github.com>
2020-11-23 02:02:52 -0500

    initial commit

---
+++ popup.js
@@ -0,0 +1,22 @@
+let scrapeButton = document.getElementById("scrapeButton")
+let statusSpan = document.getElementById("statusSpan")
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+  });
+
+
+
+  /* console.log($(".moreButton"))
+if ($(".moreButton").length > 0){
+    console.log("Ready")
+    statusSpan.textContent = "Ready"
+} else {
+    console.log("Not ready")
+    statusSpan.textContent = "Not on profile"
+}
+scrapeButton.onclick = function() {
+   null 
+} */
